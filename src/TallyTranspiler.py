56eb21de4d5a19d57597198962e83120955942ec
RyuGood0 <ryugood0@gmail.com>
2023-05-22 15:01:58 +0200

    In main

--- src/TallyTranspiler.py
+++ src/TallyTranspiler.py
@@ -1,11 +1,16 @@
 from TallyParser import TallyParser
 from dataclasses import dataclass
 
+def raise_error(comment):
+    print(comment)
+    exit()
+
 @dataclass
 class Variable:
     name: str
     type: str
     value: any
+    constant: bool = False
 
 class TallyTranspiler(object):
     parser = TallyParser()
@@ -14,16 +19,34 @@ class TallyTranspiler(object):
     variables = {}
     functions = {}
 
+    imports = []
+    func_decls = []
+
     def __call__(self, content):
         parsed = self.parser.parse(content)
 
-        c_code = ""
+        main_code = ""
 
         for statement in parsed[1]:
-            buffer = self.transpile(statement)
-            c_code += buffer
-            if not (buffer[-1] == ';' or buffer[-1] == '}'):
-                c_code += ';\n'
+            if isinstance(statement, tuple) and statement[0] != 'func_decl':
+                buffer = self.transpile(statement)
+                main_code += buffer
+                if not (buffer[-1] == ';' or buffer[-1] == '}'):
+                    main_code += ';\n'
+                else:
+                    main_code += '\n'
+
+        main_code = "".join([f"\t{code}\n" for code in main_code.splitlines()])
+
+        include_code = ""
+        for imps in self.imports:
+            include_code += f"#include <{imps}.h>\n"
+
+        func_decl_code = ""
+        for func_decl in self.func_decls:
+            func_decl_code += func_decl + "\n"
+
+        c_code = f"{include_code}\n{func_decl_code}int main(int argc, char *argv[]) {{\n{main_code}}}"
 
         return c_code
     
@@ -42,13 +65,26 @@ class TallyTranspiler(object):
             return statement
         elif statement[0] == 'assign':
             if self.is_assigned(statement)[0]:
+                if self.variables[statement[1]].constant:
+                    raise_error(f"Cannot reassign constant {self.is_assigned(statement)[1].name}")
                 return f"{self.is_assigned(statement)[1].name} = {self.transpile(statement[2])}"
             elif len(statement) == 3:
-                self.variables[statement[1]] = Variable(statement[1], 'int', 0)
-                return f"int {statement[1]} = {self.transpile(statement[2])}"
+                self.variables[statement[1]] = Variable(statement[1], 'any', 0)
+                return f"<TODO_TYPE> {statement[1]} = {self.transpile(statement[2])}"
             elif len(statement) == 4:
-                self.variables[statement[2]] = Variable(statement[2], 'int', 0)
+                self.variables[statement[2]] = Variable(statement[2], statement[1], 0)
                 return f"{self.transpile_type(statement[1])} {statement[2]} = {self.transpile(statement[3])}"
+        elif statement[0] == 'const_assign':
+            if self.is_assigned(statement)[0]:
+                raise_error(f"Constant {self.is_assigned(statement)[1].name} already assigned")
+            print(statement)
+            if len(statement) == 3:
+                self.variables[statement[1]] = Variable(statement[1], 'any', 0, True)
+                return f"const <TODO_TYPE> {statement[1]} = {self.transpile(statement[2])}"
+            elif len(statement) == 4:
+                self.variables[statement[2]] = Variable(statement[2], statement[1], 0, True)
+                return f"const {self.transpile_type(statement[1])} {statement[2]} = {self.transpile(statement[3])}"
+            
         elif statement[0] in ('+', '-', '*', '/', '**'):
             left = self.transpile(statement[1])
             right = self.transpile(statement[2])
@@ -61,6 +97,8 @@ class TallyTranspiler(object):
             elif statement[0] == '/':
                 return f"{left} / {right}"
             elif statement[0] == '**':
+                if 'math' not in self.imports:
+                    self.imports.append('math')
                 return f"pow({left}, {right})"
         elif statement[0] in ('>', '<', '>=', '<=', '=='):
             left = self.transpile(statement[1])
